029a9dc8cd79a3147c0950c08bbc5e37b43cfc14
Ira-777 <64828831+Ira-777@users.noreply.github.com>
2022-03-20 22:16:39 +0000

    Add files via upload

---
+++ BasicJavaScript/appending-variables-to-strings.js
@@ -0,0 +1,5 @@
+// Change code below this line
+
+const someAdjective = "awesome!";
+let myStr = "Learning is ";
+myStr += someAdjective;
---
+++ BasicJavaScript/build-javascript-objects.js
@@ -0,0 +1,10 @@
+const myDog = {
+  // Only change code below this line
+
+  "name": "Puppy",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Dogs", "Water"]
+
+  // Only change code above this line
+};
